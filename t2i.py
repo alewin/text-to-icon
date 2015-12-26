f561b1eb9d7f1f61bc18eaa20aca6066063e7c16
Alessio Koci <ko.alessio@gmail.com>
2015-12-26 13:16:39 +0000

    Added input-output files and progress

--- t2i.py
+++ t2i.py
@@ -1,8 +1,10 @@
-import json
 import urllib, urllib2
+import json
+import nltk
+from nltk.tag import pos_tag
 import gzip
-from textblob import TextBlob
 from StringIO import StringIO
+
 
 def get_icon(name):
     args = urllib.urlencode({'q': name, 'page': 1, 'limit': 1,'raw_html':'false'})
@@ -15,7 +17,8 @@ def get_icon(name):
     req.add_header('Referer', 'https://thenounproject.com/search/?q='+name)
     req.add_header('X-Requested-With', 'XMLHttpRequest')
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36')
-
+    
+    
     try:
         response = urllib2.urlopen(req)
         if response.info().get('Content-Encoding') == 'gzip':
@@ -26,21 +29,30 @@ def get_icon(name):
     except: 
         return ""
 
-def text2icon(text):
+def text2icon(text,tags):
     index = 0
-    tokens = TextBlob(text).noun_phrases
-    for word in tokens:
-        index = text.find(word,index,len(text))
-        icon = get_icon(word)
-        text = text[:index+len(word)] + " <img width='20' src ='" +icon +"'></img> " + text[index+len(word):]
+    progress = 0
+    ws_ts = pos_tag(text.split())
+    for word,tag in ws_ts:
+        print '%d of %d' % (progress, len(ws_ts))
+        if any(x in tag for x in tags):
+            index = text.find(word,index,len(text))
+            icon = get_icon(word)
+            text = text[:index+len(word)] + " <img width='20' src ='" +icon +"'></img> " + text[index+len(word):]
+        i += 1
     return text
 
+print("Start")
 
+fin = open("input.txt","r")
+text = fin.read()
+fin.close()
 
-text = "Here is my secret. It is very simple. It is only with the heart that one can see rightly; What is essential is invisible to the eye."
-print "\n%s"%text
-text = text2icon(text.lower())
-text = "<a>" + text + "</a>"
-print "\n%s"%text
+tags = ['NN'] #tags = ['NN,'VBP','JJ']
+result = text2icon(text,tags)
 
+fout = open("output.txt","w")
+fout.write("<a>"+result+"</a>")
+fout.close()
 
+print("End")
